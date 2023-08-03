40a929a6ed54721d56c4c0b5c30020c48faa0ab2
masanari <sad049903@gmail.com>
2023-08-03 21:28:35 +0900

    タイトル 本文

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use app\Http\Controllers\RegisterController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
